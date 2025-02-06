91e47e029e77805e782e78916e2a4c40373301e7
tinwan.ng <tinwan.ng@govmind.tech>
2025-02-06 12:47:21 +0100

    feat: add signup function

--- class_project/quiz_app/urls.py
+++ class_project/quiz_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('create', views.create_quiz, name='create_quiz'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile')
 ]
