47cc578a418844dee301650e002ae07dab42423d
tinwan.ng <tinwan.ng@govmind.tech>
2025-02-06 12:25:05 +0100

    fix: enable logout

--- class_project/class_project/settings.py
+++ class_project/class_project/settings.py
@@ -131,3 +131,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logout guide
+LOGOUT_REDIRECT_URL = '/quiz_app'
+
